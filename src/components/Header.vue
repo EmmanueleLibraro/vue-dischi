fac0244a499711d5b0d13240bf6014c6c0e55a78
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-05-06 17:23:42 +0200

    metà

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+      <div class="logo-container">
+          <img src="@/assets/logo.png" alt="spotify-logo">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'Header',
+}
+</script>
+
+<style scoped lang="scss">
+@import './scss/general.scss';
+
+
+.logo-container{
+    height: 80px;
+    background: #000;
+    display: flex;
+    align-items: center;
+}
+
+.logo-container img{
+    width: 50px;
+    margin-left: 1rem;
+}
+</style>
+
---
+++ src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+      <div class="container-main">
+          <div class="container-cards">
+              <div class="cards">
+                  cards
+              </div>
+          </div>
+      </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Main',
+    data(){
+        return {
+            apiURL: 'https://flynn.boolean.careers/exercises/api/array/music',
+            cardsList: [],
+            loading: true,
+        }
+    },
+    created(){
+        this.getCards();
+    },
+    methods: {
+        getCards(){
+            axios.get(this.apiURL)
+            .then(res =>{
+                this.cardsList = res.data;
+                this.loading = false;
+            })
+            .catch(err =>{
+                'Error' + err;
+            });
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../scss/general.scss';
+
+.container-main{
+    height: 100vh;
+    background: $second;
+}
+
+.container-cards{
+    width: 1170px;
+    display: flex;
+}
+</style>
